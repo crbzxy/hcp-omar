b52b55a160e28a705d443e1bdaca9e428e94075d
crbzxy <carlos.boor@gmail.com>
2023-01-07 19:24:41 -0600

    Update

--- components/benefits.tsx
+++ components/benefits.tsx
@@ -10,47 +10,47 @@ const beneftis = [
   {
     title: 'Cirugía General     ',
     description: 'Centro Médico ISSEMYM. Toluca.',
-    image: 'icon_bulb',
+    image: '4',
   },
   {
     title: 'Médico Cirujano',
     description: 'Universidad Autónoma del Estado de México',
-    image: 'icon_db',
+    image: '5',
   },
   {
     title: 'Preceptorship en Andrología',
     description: 'Fundación Puigvert en Barcelona, España',
-    image: 'icon_lock',
+    image: '11',
   },
   {
     title: 'Socio Titular',
     description: 'Sociedad Mexicana de Urología',
-    image: 'icon_lock',
+    image: '8',
   },
   {
     title: 'Alta especialidad en Urología Ocológica',
     description: 'Instituto nacional de Cancerología',
-    image: 'icon_lock',
+    image: '1',
   },
   {
     title: 'Alta especialidad en Medicina, Urología Oncológica',
     description: 'Universidad Autónoma del Estado de México',
-    image: 'icon_lock',
+    image: '5',
   },
   {
     title: 'Especialidad en Urología',
     description: 'Consejo Nacional Mexicano de Urología',
-    image: 'icon_lock',
+    image: '9',
   },
   {
     title: 'Especialidad en Urología',
     description: 'Universidad Autónoma del Estado de México',
-    image: 'icon_lock',
+    image: '5',
   },
   {
     title: 'Profesor de la acedemia de urología de la Facultad de Medicina',
     description: 'Universidad Autónoma de México',
-    image: 'icon_lock',
+    image: '10',
   },
 ];
 
@@ -97,9 +97,9 @@ export default function Benefits() {
               className={styles.card}>
               <div className={styles.Image}>
                 <Image
-                  width={15}
-                  height={15}
-                  src={`/${item.image}.svg`}
+                  width={80}
+                  height={80}
+                  src={`/formacion${item.image}.svg`}
                   alt={item.title}
                 />
               </div>
